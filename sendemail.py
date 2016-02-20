2e10478200fc934095bf7444518b67126141bd1a
Renaldo Pringle <rapchristoff@gmail.com>
2016-02-20 13:21:28 -0500

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -1,1 +1,43 @@
+import smtplib
 
+fromaddr = 'pigeonflight@gmail.com'
+
+toaddr  = 'david@alteroo.com'
+
+message = """From: {} <{}>
+
+To: {} <{}>
+
+Subject: {}
+
+{}""" messagetosend = message.format(
+
+                             fromname,
+
+                             fromaddr,
+
+                             toname,
+
+                             toaddr,
+
+                             subject,
+
+                             msg)
+
+# Credentials (if needed)
+
+username = 'pigeonflight@gmail.com'
+
+password = '{youremailapppassword}''
+
+# The actual mail send
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+
+server.starttls()
+
+server.login(username,password)
+
+server.sendmail(fromaddr, toaddrs, messagetosend)
+
+server.quit()
